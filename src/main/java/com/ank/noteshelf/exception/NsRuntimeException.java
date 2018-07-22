9065ec9a284d8a44eb972d683f3d8c6f007622f0
Rai, Ankit(ankirai) <ankirai@paypal.com>
2018-07-22 12:10:51 +0530

    Exception handling refactored

---
+++ src/main/java/com/ank/noteshelf/exception/NsRuntimeException.java
@@ -0,0 +1,22 @@
+package com.ank.noteshelf.exception;
+
+public class NsRuntimeException extends RuntimeException {
+
+	private static final long serialVersionUID = 5861310537366287163L;
+
+    public NsRuntimeException() {
+        super();
+    }
+
+    public NsRuntimeException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public NsRuntimeException(final String message) {
+        super(message);
+    }
+
+    public NsRuntimeException(final Throwable cause) {
+        super(cause);
+    }
+}
